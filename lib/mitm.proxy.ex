51e805da44d3a573cc9e045e4d3978cb454efa2d
lissana <ktion23@gmail.com>
2023-11-28 22:13:33 +0800

    mitm.proxy.ex

--- lib/mitm.proxy.ex
+++ lib/mitm.proxy.ex
@@ -24,7 +24,7 @@ defmodule Mitme.Acceptor.Supervisor do
 
     IO.inspect(children)
 
-    supervise(children, strategy: :one_for_one)
+    supervise(children, [strategy: :one_for_one, max_restarts: 10000, seconds: 1])
   end
 end
 
